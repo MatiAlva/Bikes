664d41f60b425a5d24d15cdb0cb64564897cb79d
matias alvarez <MatiAlva>
2021-10-14 07:44:50 -0300

    Error en el responsive de los iphone, pero en Netlify

---
+++ Script/index.js
@@ -0,0 +1,8 @@
+    //Parallax
+let parallax = document.querySelector('.parallax');
+
+window.onscroll = () => {
+    let scrollTop = document.documentElement.scrollTop;
+    parallax.style.backgroundPositionY = scrollTop * 0.7 + 'px';
+}
+
